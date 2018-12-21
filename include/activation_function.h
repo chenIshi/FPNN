3708cd82048f5a228d151ca9f80db69ca50b1e13
David <jesus255221@gmail.com>
2018-12-21 15:07:31 +0800

    [UPDATE] Add template

--- include/activation_function.h
+++ include/activation_function.h
@@ -1,5 +1,8 @@
+#ifndef ACTIVATION_FUNCTION_H
+#define ACTIVATION_FUNCTION_H
 #include "numerical.h"
 #include "error_handle.h"
+#define comb(X, Y) X##_##Y
 
 size_t activation_function_float(void *input,
                                  void *output,
@@ -11,6 +14,7 @@ size_t activation_function_fixed(void *input,
                                  void *(*func_ptr)(void *),
                                  enum error_code *err);
 
-void *relu_float(void *input);
+void *relu_float(void *input, void *output);
 
-void *relu_fixed(void *input);
+void *relu_fixed(void *input, void *output);
+#endif
--- include/activation_function_template.h
+++ include/activation_function_template.h
@@ -1,36 +1,32 @@
-#include "numerical.h"
-#include "error_handle.h"
-
-#define comb(X, Y) X_##Y
-
-#ifndef numtype
-#define numtype unsigned
+#if NUMTYPE == float
+#define DATA_TYPE float
+#elif NUMTYPE == fixed
+#define DATA_TYPE unsigned
 #endif
 
-#if numtype == float
-#define num_t float
-#endif
+#define activation_function comb(activation_function, NUMTYPE)
+#define relu comb(relu, NUMTYPE)
 
-#if numtype == fixed
-#define num_t unsigned
-#endif
-
-#define activation_function comb(activation_function, numtype)
 
 size_t activation_function(void *input, void *output, void *(*func_ptr)(void *, void *), enum error_code *err)
 {
     output = func_ptr(input, output);
+    return 0;
 }
 
-#define relu comb(relu, numtype)
 
-void *relu(void *input, void *output) {
-    if ((*(num_t *)input) < 0) {
-        *(num_t *)output = 0;
+
+void *relu(void *input, void *output)
+{
+    if ((*(DATA_TYPE *)input) < 0)
+    {
+        *(DATA_TYPE *)output = 0;
     }
     else
         return output;
 }
 
-#undef numtype
-#undef num_t
+#undef NUMTYPE
+#undef DATA_TYPE
+#undef activation_function
+#undef relu
